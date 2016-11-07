8d7db7c743d1e5ac8eb3f94b247e6b5413245db2
Jesse Novotny <jessenovotny@gmail.com>
2016-11-07 09:35:54 -0700

    Done.

--- tests/FavoriteFood.spec.js
+++ tests/FavoriteFood.spec.js
@@ -1,11 +1,25 @@
 describe('Favorite Food Filter', function () {
-	var $controller;
+	var $controller, favoriteFood;
 
 	beforeEach(module('app'));
 
-	beforeEach(inject(function ($injector) {
-		$filter = $injector.get('$filter');
+	beforeEach(inject(function ($filter) {
+		favoriteFood = $filter('favoriteFood');
 	}));
 
+  it('should filter foods by favorite', function() {
+    var mockedList = [{
+      name: 'Test 1',
+      favoriteFood: 'Cheeseburger'
+    }, {
+      name: 'Test 2',
+      favoriteFood: 'Pizza'
+    }, {
+      name: 'Test 3',
+      favoriteFood: 'Hotdog'
+    }]
+
+    expect(favoriteFood(mockedList, 'Pizza').length).toEqual(1);
+  })
 	
 });
--- tests/removeAllVowels.spec.js
+++ tests/removeAllVowels.spec.js
@@ -1,10 +1,14 @@
 describe('removeAllVowels Filter', function () {
-	var $controller;
+	var $controller, removeAllVowels;
 
 	beforeEach(module('app'));
 
-	beforeEach(inject(function ($injector) {
-		$filter = $injector.get('$filter');
+	beforeEach(inject(function ($filter) {
+		removeAllVowels = $filter('removeAllVowels');
 	}));
+
+  it('should remove all vowels from a string', function() {
+    expect(removeAllVowels('hello')).toEqual('hll');
+  })
 
 });
